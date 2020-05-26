8cadf5a1c13b3ac712be08bbeb1b8c18581d82db
Sara Zavala <saritazg2010@gmail.com>
2020-05-26 16:00:38 -0600

    home component

--- ReadHub/components/Home/index.js
+++ ReadHub/components/Home/index.js
@@ -1,42 +1,236 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {View, TextInput, StyleSheet, Button, Text, Alert} from 'react-native';
-import { connect } from 'react-redux';
-//Testing git pushes
+import {Field, reduxForm} from 'redux-form';
+import {
+  View,
+  TextInput,
+  ScrollView,
+  Text,
+  StyleSheet,
+  Alert,
+  Button,
+  ImageBackground,
+  Image,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard
+  
+} from 'react-native';
 
-const test = () =>{
-    return (
-    <View style={styles.container}>
-    <Button title="hahaha" />
-    <Text style={styles.container} >NO POS MIAU</Text>
-    </View>
-);
+import {connect} from 'react-redux';
+
+
+const Home = () => {
+  return (
+ 
+       <ScrollView >
+          <View  style={styles.container} >
+            <Image
+              style={styles.image}
+              source={require('./Images/logo.png')}
+            />
+          </View>
+
+          <View
+            style={{
+              flex: 1,
+              //flexDirection: 'row',
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              alignItems: 'flex-start',
+              height: 250,
+              //backgroundColor:'red'
+            }}>
+            <View style={styles.inputWrap}>
+
+            <TouchableOpacity style={styles.button} onPress={()=>{alert("you clicked me")}}>
+                <Text style={styles.text}>
+                    Comprar un libro
+                 </Text>
+  
+                 <Image 
+                    style={styles.imageIcon}
+                    source={require("./Images/Comprar.png")}
+                    />
+                </TouchableOpacity>
+
+            </View>
+
+            <View style={styles.inputWrap}>
+            <TouchableOpacity style={styles.buttonRight} onPress={()=>{alert("you clicked me")}}>
+              <Text style={styles.text2}>
+                Prestar un libro
+              </Text>
+               <Image 
+                style={styles.imageIconRight}
+                source={require("./Images/prestarLibro.png")}
+               />
+            </TouchableOpacity>
+            </View>
+          </View>
+
+          <View
+            style={{
+              flex: 1,
+              //flexDirection: 'row',
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              alignItems: 'flex-start',
+              height: 250,
+              //backgroundColor:'red'
+            }}>
+            <View style={styles.inputWrap}>
+
+            <TouchableOpacity style={styles.button} onPress={()=>{alert("you clicked me")}}>
+                <Text style={styles.text}>
+                    Reservar salon
+                 </Text>
+  
+                 <Image 
+                    style={styles.imageIcon}
+                    source={require("./Images/esta.png")}
+                    />
+                </TouchableOpacity>
+
+            </View>
+
+            <View style={styles.inputWrap}>
+            <TouchableOpacity style={styles.buttonRight} onPress={()=>{alert("you clicked me")}}>
+              <Text style={styles.text3}>
+                Solicitar Equipo
+              </Text>
+               <Image 
+                style={styles.imageIconRight}
+                source={require("./Images/images.png")}
+               />
+            </TouchableOpacity>
+            </View>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              //flexDirection: 'row',
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              alignItems: 'flex-start',
+              height: 250,
+              //backgroundColor:'red'
+            }}>
+            <View style={styles.inputWrap}>
+
+            <TouchableOpacity style={styles.button} onPress={()=>{alert("you clicked me")}}>
+                <Text style={styles.text}>
+                    Comprar revistas
+                 </Text>
+  
+                 <Image 
+                    style={styles.imageIconMagazine}
+                    source={require("./Images/magazine.png")}
+                    />
+                </TouchableOpacity>
+
+            </View>
+
+            <View style={styles.inputWrap}>
+            <TouchableOpacity style={styles.buttonRight} onPress={()=>{alert("you clicked me")}}>
+              <Text style={styles.text2}>
+                Prestar un libro
+              </Text>
+               <Image 
+                style={styles.imageIconRight}
+                source={require("./Images/prestarLibro.png")}
+               />
+            </TouchableOpacity>
+            </View>
+          </View>
+        
+        </ScrollView>
+
+  )
 };
 
 const styles = StyleSheet.create({
-    button: {
-      backgroundColor: 'rebeccapurple',
-      color: 'white',
-      height: 40,
-      lineHeight: 30,
-      marginTop: 10,
-      textAlign: 'center',
-      width: 250,
-      marginLeft : 50,
-    },
+  buttonRight:{
+    height: 250,
+    width: 160,
+    marginTop:40,
+    marginLeft: 5
+  },
+  imageIconMagazine:{
+    height:150,
+    width: 150,
+    borderRadius: 20,
+    marginLeft: 5
+
+  },
+
+  inputWrap: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+
+  imageIconRight:{
+    height:150,
+    width: 150,
+    marginTop: 5,
+    marginLeft: 15
+
+  },
+  text:{
+    fontFamily: "Cochin",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 0,
+    marginBottom: 5
+    
+  },
+  text2:{
+    fontFamily: "Cochin",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 10,
+    marginLeft: 20
+    
+  },
+  text3:{
+    fontFamily: "Cochin",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 20,
+    textAlign: 'center'
+    
+  },
+  
+  
+  imageIcon:{
+    height:150,
+    width: 150,
+    borderRadius: 45
+  },
+
+  button: {
+    height:155,
+    width: 160,
+    marginTop: 50,
+    marginLeft: 25,
+    //backgroundColor: 'blue'
+  },
+  image: {
+    width: 400,
+    height: 100,
+    //marginRight:500,
+    //backgroundColor : 'blue'  
+  },
     container: {
-      marginTop: 300,
-      marginLeft: 100,
-      color: 'red',
-      fontSize:40,
-    },
-    input: {
-      borderColor: 'black',
-      borderWidth: 1,
-      height: 37,
-      width: 250,
-      marginLeft : 50,
-      marginBottom: 50,
-    },
-  });
-export default connect()(test);
+    //backgroundColor: 'red',
+    marginTop: 100,
+    marginRight: 25,
+    marginLeft: 15,
+   // backgroundColor: 'red'
+  },
+  
+
+});
+
+export default Home;
